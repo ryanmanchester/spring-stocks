106735ef153491ca4d56ebc03bf5b63fe6e5466e
Ryan Manchester <ryanm17@gmail.com>
2024-02-21 14:30:52 -0500

    adds json property annotation for stock service

--- spring-stocks/src/main/java/ryanmanchester/info/springstocks/stock/StockService.java
+++ spring-stocks/src/main/java/ryanmanchester/info/springstocks/stock/StockService.java
@@ -3,9 +3,13 @@ package ryanmanchester.info.springstocks.stock;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 @Service
 public class StockService {
+	@JsonProperty("01. symbol")
 	private String alphaVantageApiKey;
+	@JsonProperty("05. price")
 	private String alphaVantageBaseUrl;
 	
 	private final RestTemplate restTemplate;
