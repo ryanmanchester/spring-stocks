5fa5176edd399ec931bddc0ef9bc4a07e43aef07
Ryan Manchester <ryanm17@gmail.com>
2024-02-21 15:17:47 -0500

    gets rest template working via rest template builder

--- spring-stocks/src/main/java/ryanmanchester/info/springstocks/stock/StockService.java
+++ spring-stocks/src/main/java/ryanmanchester/info/springstocks/stock/StockService.java
@@ -1,7 +1,9 @@
 package ryanmanchester.info.springstocks.stock;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class StockService {
@@ -10,5 +12,23 @@ public class StockService {
 	
 	@Value("${alphavantage.base.url}")
 	private String alphaVantageBaseUrl;
+	
+	private final RestTemplate restTemplate;
+	
+	public StockService(RestTemplateBuilder restTemplateBuilder) {
+		this.restTemplate = restTemplateBuilder.build();
+	}
+	
+	public Stock getStockData(String symbol) {
+		String url = alphaVantageBaseUrl + "?function=GLOBAL_QUOTES$symbol="+symbol+"&apikey=" +alphaVantageApiKey;
+		try {
+			Stock stockData = restTemplate.getForObject(url, Stock.class);
+			return stockData;
+			
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return null;
+	}
 
 }
