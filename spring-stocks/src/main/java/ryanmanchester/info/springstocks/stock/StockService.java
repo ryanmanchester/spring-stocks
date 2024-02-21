ef07d97b6da18997b3652b37d9f830561bd7e227
Ryan Manchester <ryanm17@gmail.com>
2024-02-21 14:44:55 -0500

    implements basic get request to alpha vantage api

--- spring-stocks/src/main/java/ryanmanchester/info/springstocks/stock/StockService.java
+++ spring-stocks/src/main/java/ryanmanchester/info/springstocks/stock/StockService.java
@@ -1,19 +1,34 @@
 package ryanmanchester.info.springstocks.stock;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class StockService {
-	
+	@Value("${alphavantage.api.key}")
 	private String alphaVantageApiKey;
-	
+	@Value("${alphavantage.base.url}")
 	private String alphaVantageBaseUrl;
 	
 	private final RestTemplate restTemplate;
 	
 	public StockService(RestTemplate restTemplate) {
 		this.restTemplate = restTemplate;
+	}
+	
+	public Stock stockData(String symbol) {
+		String url = alphaVantageBaseUrl + "?function=GLOBAL_QUOTE&symbol="+symbol+"&apikey="+alphaVantageApiKey;
+		try {
+			Stock stock = restTemplate.getForObject(url, Stock.class);
+			if(stock != null) {
+				return stock;
+			}
+		} catch(Exception e) {
+			e.getMessage();
+			
+		}
+		return null;
 	}
 
 }
