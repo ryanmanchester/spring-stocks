d038fac299005748a7ef9f8abab5e06f12509ba1
Ryan Manchester <ryanm17@gmail.com>
2024-02-21 15:10:36 -0500

    improved stock service implementation

--- spring-stocks/src/main/java/ryanmanchester/info/springstocks/stock/Stock.java
+++ spring-stocks/src/main/java/ryanmanchester/info/springstocks/stock/Stock.java
@@ -3,25 +3,16 @@ package ryanmanchester.info.springstocks.stock;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Stock {
-	
 	@JsonProperty("01. symbol")
 	private String symbol;
 	@JsonProperty("05. price")
 	private String price;
-	
-	public Stock(String symbol, String price) {
-		this.symbol = symbol;
-		this.price = price;
-	}
-
 	public String getSymbol() {
 		return symbol;
 	}
-
 	public String getPrice() {
 		return price;
 	}
 	
 	
-
 }
